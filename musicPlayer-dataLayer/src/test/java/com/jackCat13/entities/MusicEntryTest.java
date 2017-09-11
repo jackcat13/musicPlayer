fc3dbda4b8da4fa639c285ce209eed44573df349
jackCat13 <christophehenry11@gmail.com>
2017-09-11 19:55:24 +0200

    Test written to validate MusicEntry entity. But no able to find persistence provider

--- musicPlayer-dataLayer/src/test/java/com/jackCat13/entities/MusicEntryTest.java
+++ musicPlayer-dataLayer/src/test/java/com/jackCat13/entities/MusicEntryTest.java
@@ -2,13 +2,28 @@ package com.jackCat13.entities;
 
 import static org.junit.Assert.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.Test;
 
 public class MusicEntryTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("MPPU");
+        EntityManager theManager = factory.createEntityManager();
+        theManager.getTransaction().begin();
+
+        MusicEntry musicEntry = new MusicEntry();
+        musicEntry.setMusicPath("pathTest");
+        theManager.persist(musicEntry);
+
+        MusicEntry musicRetrieved = (MusicEntry)theManager.find(MusicEntry.class, "pathTest");
+
+        assertEquals("pathTest", musicRetrieved.getMusicPath()); 
+        theManager.close();
 	}
 
 }
